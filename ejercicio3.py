c363897b800ae85c7db45a2910d0a11c459193d7
guido102002 <162534248+guido102002@users.noreply.github.com>
2025-03-08 16:47:20 -0500

    desarrollo tres y cuatro

---
+++ ejercicio3.py
@@ -0,0 +1,47 @@
+#Transforme la cadena El mejor regalo? El perdón... en el mejor perdón utilizando 
+
+#únicamente los métodos de listas que hemos visto. La nueva cadena debe guardarse en la variable nueva_frase:
+
+
+
+def TransformarTexto():
+
+    texto = "El mejor regalo? El perdón..."
+
+    lista=texto.split()
+
+    print(lista)
+
+    
+
+    lista.remove("regalo?")
+
+    lista.pop(2)
+
+    lista[0] = lista[0].lower()
+
+    print(lista)
+
+    
+
+    #Se convierte la lista de palabras a lista de caracteres para 
+
+    nueva_lista=[list(palabra) for palabra in lista]
+
+    print(nueva_lista)
+
+    #Se eliminan los puntos (...)
+
+    nueva_lista[-1] = [caracter for caracter in nueva_lista[-1] if caracter not in ['.', '...']]
+
+    print(nueva_lista)
+
+    
+
+    nueva_frase = " ".join(["".join(palabra) for palabra in nueva_lista])
+
+    print(nueva_frase)
+
+    
+
+TransformarTexto()
---
+++ ejercicio4.py
@@ -0,0 +1,35 @@
+import random
+
+def generar_lista(n,minimo,maximo):
+    return[random.randint(minimo,maximo) for _ in range(n)]
+#Funcion para sacar el cuadrado y el cubo de la lista random
+
+def Cuadrado_Cubo(lista):
+
+    listCuadrado = []
+
+    listCubo = []
+
+    for num in lista:
+
+        listCuadrado.append(num**2)
+
+        listCubo.append(num**3)
+
+    print("Lista de Cuadrado: ",listCuadrado)
+
+    print("Lista de Cuadrado: ",listCubo)
+
+
+
+# Inicializar lista con 10 valores aleatorios entre 1 y 10
+
+lista_numeros = generar_lista(10, 1, 10)
+
+print("Lista de Números: ",lista_numeros)
+
+
+
+# Mostrar los valores con sus cuadrados y cubos
+
+Cuadrado_Cubo(lista_numeros)
